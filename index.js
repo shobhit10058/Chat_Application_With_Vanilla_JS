30ed30b401a69cca0f96e240105ec809dde34356
Shobhit Gupta <rkgupta462gkp@gmail.com>
2022-05-25 01:01:09 +0530

    Made a Basic Chat Application with mqtt and vanilla js

---
+++ index.js
@@ -0,0 +1,87 @@
+const HOST = "broker.emqx.io";
+const PORT = 8083;
+const CHANNEL_NAME = "main";
+
+const inputMessage = document.querySelector(".chat_container input");
+const messageContainer = document.querySelector(".message_container");
+const usernameInput = document.querySelector(".username_form input");
+const connectButton = document.querySelector(".username_form button");
+
+inputMessage.setAttribute("disabled", true);
+connectButton.setAttribute("disabled", true);
+
+const clientId = "mqttjs_" + Math.random().toString(16).substring(2, 8);
+
+const host = "ws://broker.emqx.io:8083/mqtt";
+
+const options = {
+  keepalive: 60,
+  clientId: clientId,
+  protocolId: "MQTT",
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 1000,
+  connectTimeout: 30 * 1000,
+  will: {
+    topic: "WillMsg",
+    payload: "Connection Closed abnormally..!",
+    qos: 0,
+    retain: false,
+  },
+};
+
+console.log("Connecting mqtt client");
+const client = mqtt.connect(host, options);
+
+client.on("error", (err) => {
+  console.log("Connection error: ", err);
+  client.end();
+});
+
+client.on("reconnect", () => {
+  console.log("Reconnecting...");
+});
+
+client.on("connect", () => {
+  console.log("Client connected: " + clientId);
+  // Subscribe
+  client.subscribe(CHANNEL_NAME, { qos: 0 });
+  connectButton.removeAttribute("disabled");
+  // Publish
+});
+
+// Received
+client.on("message", (topic, message, packet) => {
+  console.log(
+    "Received Message: " + message.toString() + "\nOn topic: " + topic
+  );
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = message.toString();
+  messageContainer.append(newMessage);
+});
+
+const sendMessage = (channelName, message) => {
+  client.publish(channelName, message, {
+    qos: 0,
+    retain: false,
+  });
+  inputMessage.value = "";
+};
+
+const startChat = () => {
+  inputMessage.removeAttribute("disabled");
+  inputMessage.addEventListener("change", (event) => {
+    sendMessage(
+      CHANNEL_NAME,
+      `<strong>${usernameInput.value.toString()}</strong>: ${
+        event.target.value
+      }`
+    );
+  });
+  sendMessage(
+    CHANNEL_NAME,
+    `<strong>${usernameInput.value.toString()}</strong> has joined!`
+  );
+};
+
+connectButton.addEventListener("click", startChat);
